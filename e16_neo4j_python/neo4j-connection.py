a34f806e88f8dcc6f4a5434c6a2b16915f6264ec
Moises Alonso <maalonso@uvg.edu.gt>
2025-04-30 10:48:55 -0600

    Ejemplo conexion python

---
+++ e16_neo4j_python/neo4j-connection.py
@@ -0,0 +1,19 @@
+from neo4j import GraphDatabase
+
+uri = "bolt://localhost:7687"
+user = "javaapp"
+password = "Test2025!"
+
+driver = GraphDatabase.driver(uri, auth=(user, password))
+
+def run_query(query):
+    with driver.session() as session:
+        result = session.run(query)
+        records = list(result)
+        return records
+    
+query = "MATCH (pelicula:Movie) RETURN pelicula.title AS title"
+result = run_query(query)
+
+for record in result:
+    print(record["title"])
